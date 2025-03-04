04f634ba393dd643052693def60c359c3c565c10
Shobhit Jain <shobhitjain@Shobhits-MacBook-Air.local>
2025-03-04 13:56:04 +0530

    episode-01

--- App.js
+++ App.js
@@ -1,26 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-/* 
-
-<div id="parent">
-  <div id=child>
-    <h1>I'm h1 Tag</h1>
-    <h2>I'm h2 Tag</h2>
-  </div>
-  <div id=child2>
-    <h1>I'm h1 Tag</h1>
-    <h2>I'm h2 Tag</h2>
-  </div>
-</div>
-
-ReactElement(Object) => HTML(Browser Understands)
-
-*/
-
 const parent = React.createElement('div', { id: 'parent' }, [
   React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, 'This is Namaste React 🚀'),
+    React.createElement('h1', {}, "I'm h1 Tag"),
     React.createElement('h2', {}, "I'm h2 Tag"),
   ]),
   React.createElement('div', { id: 'child2' }, [
@@ -28,7 +8,13 @@ const parent = React.createElement('div', { id: 'parent' }, [
     React.createElement('h2', {}, "I'm h2 Tag"),
   ]),
 ]);
-const root = ReactDOM.createRoot(document.getElementById('root')); // It's the job of ReactDOM to create root i.e, heading in this case, bcz root is a dom element which is rendered to be on browser. This root is the place where all React code will run.
+
+// JSX
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // root.render(heading);
 root.render(parent);
+
